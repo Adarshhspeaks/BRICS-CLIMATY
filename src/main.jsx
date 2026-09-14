f5cbaf5d358bac45727eb39fb4d1bfb9b5ecb6b8
BRICS-CLIMATY <contact@brics-climaty.org>
2026-09-14 17:41:34 +0530

    chore: install and configure map dependencies and default Leaflet assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import App from './App';
 import './index.css';
+
+// Fix for Leaflet default icon asset paths in Vite
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
